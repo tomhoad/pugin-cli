0f2d0dd2f5485a6386bb8842d65e45f7588d2a82
Tom Hoad <hoadt@parliament.uk>
2022-10-07 11:58:34 +0100

    feat: fetch divisions api, add ts

--- bin/pugin.js
+++ bin/pugin.js
@@ -1,7 +1,23 @@
 #! /usr/bin/env node
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const commander_1 = require("commander");
-commander_1.program.command("divisions").description("Displays a list of recent divisions");
-commander_1.program.parse();
-console.log("Here is a list of divisions");
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+import { program } from "commander";
+import got from "got";
+program
+    .command("divisions")
+    .description("Displays a list of recent divisions")
+    .action(() => __awaiter(void 0, void 0, void 0, function* () {
+    const res = yield got.get("https://commonsvotes-api.parliament.uk/data/divisions.json/search", { responseType: "json" });
+    const divisions = res.body;
+    for (const division of divisions) {
+        console.log(division.Title);
+    }
+}));
+program.parse();
--- src/pugin.ts
+++ src/pugin.ts
@@ -1,9 +1,26 @@
 #! /usr/bin/env node
 
 import { program } from "commander";
+import got from "got";
 
-program.command("divisions").description("Displays a list of recent divisions");
+interface Division {
+  Title: string;
+}
+
+program
+  .command("divisions")
+  .description("Displays a list of recent divisions")
+  .action(async () => {
+    const res = await got.get<Division[]>(
+      "https://commonsvotes-api.parliament.uk/data/divisions.json/search",
+      { responseType: "json" }
+    );
+
+    const divisions = res.body;
+
+    for (const division of divisions) {
+      console.log(division.Title);
+    }
+  });
 
 program.parse();
-
-console.log("Here is a list of divisions");
